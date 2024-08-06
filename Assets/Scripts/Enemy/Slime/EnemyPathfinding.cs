9aaa0113205a327b0aa288ad08a87302d90ce1c6
Proxiermax <teethawat9438@gmail.com>
2024-08-07 01:51:48 +0700

    Initial commit

---
+++ Assets/Scripts/Enemy/Slime/EnemyPathfinding.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+    private Knockback knockback;
+    private Animator enemyAnimator;
+    private SpriteRenderer enemySpriteRender;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        knockback = GetComponent<Knockback>();
+        enemyAnimator = GetComponent<Animator>();
+        enemySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (knockback.GettingKnockedBack) { return; }
+
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+        AdjustEnemyFacingDirection();
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDir = targetPosition;
+
+        enemyAnimator.SetFloat("moveX", moveDir.x);
+        enemyAnimator.SetFloat("moveY", moveDir.y);
+    }
+
+    private void AdjustEnemyFacingDirection()
+    {
+        if (moveDir.x < 0)
+        {
+            enemySpriteRender.flipX = true;
+        }
+        else
+        {
+            enemySpriteRender.flipX = false;
+        }
+    }
+}
